6bf10e11236fcfac9145247426a975f63ef4b444
Johannes Heesterman <johannes@elfsquad.io>
2022-06-08 20:10:36 +0200

    Run flux file in WASM

--- web/go.mod
+++ web/go.mod
@@ -2,5 +2,9 @@ module github.com/johannesheesterman/flux/web
 
 go 1.18
 
-require github.com/johannesheesterman/flux/parser v0.0.0
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220418222510-f25a4f6275ed
+	github.com/johannesheesterman/flux/parser v0.0.0
+)
+
 replace github.com/johannesheesterman/flux/parser => ../parser/src
--- web/src/main.go
+++ web/src/main.go
@@ -1,22 +1,42 @@
 package main
 
-import (  
-    "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
 	"syscall/js"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/johannesheesterman/flux/parser"
+	"github.com/johannesheesterman/flux/parser/lang"
 )
 
 func runFlux() js.Func {
 	return js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		if len(args) != 1 { 
+		if len(args) != 1 {
 			return "Invalid no of arguments passed"
 		}
 
-		fmt.Println("Run Flux!")
+		go func() {
+			resp, _ := http.Get(args[0].String())
+			body, _ := io.ReadAll(resp.Body)
+			fmt.Println(string(body))
+
+			input := antlr.NewInputStream(string(body))
+			lexer := lang.NewFluxLexer(input)
+			stream := antlr.NewCommonTokenStream(lexer, 0)
+			p := lang.NewFluxParser(stream)
+			p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+			p.BuildParseTrees = true
+			tree := p.Prog()
+			parser.NewFluxVistor().Visit(tree)
+		}()
+
 		return nil
 	})
 }
 
-func main() {  
-    js.Global().Set("runFlux", runFlux())
-	<-make(chan bool) // Keep program running
+func main() {
+	js.Global().Set("runFlux", runFlux())
+	select {}
 }
